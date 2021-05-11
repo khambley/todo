e94a4d4eab7f70d37dd3eb59dd400e7b7d26325a
Katherine Hambley <superdesigngirl@mac.com>
2021-05-11 16:30:55 -0500

    filtering out completed items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,11 +19,10 @@ export class AppComponent {
   }
 
   get itemCount(): number {
-    return this.list.items
-      .filter(item => !item.complete).length;
+    return this.items.length;   
   }
 
   get items(): readonly TodoItem[] {
-    return this.list.items;
+    return this.list.items.filter(item => !item.complete);
   }
 }
